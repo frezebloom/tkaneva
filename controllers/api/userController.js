71ddae7b43b14be50b60b655061e7b18f0412ab7
juxx <juxx@bk.ru>
2019-02-14 10:41:48 +0300

    сохранение юзера в бд

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,6 +1,17 @@
+const db = require("../../models/index");
+const User = db.user;
+
 module.exports = {
   create(req, res) {
     const { firstname, lastname, email, group, password } = req.body.user;
-    console.log(email);
+    User.create({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      group: group,
+      password: password
+    }).then(user => {
+      res.send(user);
+    });
   }
 };
