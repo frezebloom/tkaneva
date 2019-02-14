d18204845b8744bc1320299d2ec3e0355bf06de6
juxx <juxx@bk.ru>
2019-02-14 16:57:39 +0300

    шифрование пароль юзера

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,15 +1,20 @@
+var bcrypt = require("bcrypt");
 const db = require("../../models/index");
 const User = db.user;
 
 module.exports = {
   create(req, res) {
     const { firstname, lastname, email, group, password } = req.body.user;
+
+    const salt = bcrypt.genSaltSync(10);
+    const passwordToSave = bcrypt.hashSync(password, salt);
+
     User.create({
       firstname: firstname,
       lastname: lastname,
       email: email,
       group: group,
-      password: password
+      password: passwordToSave
     }).then(user => {
       res.send(user);
     });
