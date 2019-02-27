e9f0e54a7b09c20a0520be7c1966aed7e711c204
juxx <juxx@bk.ru>
2019-02-27 15:23:43 +0300

    save

--- dev/manager/src/App.vue
+++ dev/manager/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <Header/>
+    <div v-if="isAuthenticated">
+      <Header/>
+    </div>
     <div class="wrapper">
-      <Sidebar/>
+      <div v-if="isAuthenticated">
+        <Sidebar/>
+      </div>
+
       <Content/>
     </div>
   </div>
@@ -12,17 +17,22 @@ import Header from "@/components/Header.vue";
 import Sidebar from "@/components/Sidebar.vue";
 import Content from "@/components/Content.vue";
 
+import { mapGetters } from "vuex";
+
 export default {
   name: "App",
   components: {
     Header,
     Sidebar,
     Content
+  },
+  computed: {
+    ...mapGetters(["isAuthenticated"])
   }
 };
 </script>
 <style lang="scss">
-@import "../styles/reset.scss";
+@import "./styles/reset.scss";
 #app {
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
   background-color: #f4f4f4;
