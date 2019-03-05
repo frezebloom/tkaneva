134fc06561e1cbbc993acb3e8f696179c562332e
juxx <juxx@bk.ru>
2019-03-05 13:46:58 +0300

    inputHandler

--- dev/manager/src/components/UserForm.vue
+++ dev/manager/src/components/UserForm.vue
@@ -4,31 +4,66 @@
       <div class="form-header">Создать акаунт</div>
       <div class="form-wrapper">
         <label class="form-label">Имя</label>
-        <input :value="state.firstname" class="form-input" type="text">
+        <input
+          :value="state.firstname"
+          @input="inputHandler($event, 'firstname')"
+          class="form-input"
+          type="text"
+        >
       </div>
       <div class="form-wrapper">
         <label class="form-label">Фамилия</label>
-        <input :value="state.lastname" class="form-input" type="text">
+        <input
+          :value="state.lastname"
+          @input="inputHandler($event, 'lastname')"
+          class="form-input"
+          type="text"
+        >
       </div>
       <div class="form-wrapper">
         <label class="form-label">Логин</label>
-        <input :value="state.login" class="form-input" type="text">
+        <input
+          :value="state.login"
+          @input="inputHandler($event, 'login')"
+          class="form-input"
+          type="text"
+        >
       </div>
       <div class="form-wrapper">
         <label class="form-label">Почта</label>
-        <input :value="state.email" class="form-input" type="text">
+        <input
+          :value="state.email"
+          @input="inputHandler($event, 'email')"
+          class="form-input"
+          type="text"
+        >
       </div>
       <div class="form-wrapper">
         <label class="form-label">Группа</label>
-        <input :value="state.group" class="form-input" type="select">
+        <input
+          :value="state.group"
+          @input="inputHandler($event, 'group')"
+          class="form-input"
+          type="select"
+        >
       </div>
       <div class="form-wrapper">
         <label class="form-label">Пароль</label>
-        <input :value="state.password" class="form-input" type="password">
+        <input
+          :value="state.password"
+          @input="inputHandler($event, 'password')"
+          class="form-input"
+          type="password"
+        >
       </div>
       <div class="form-wrapper">
         <label class="form-label">Повторите пароль</label>
-        <input :value="state.confrimPassword" class="form-input" type="password">
+        <input
+          :value="state.confrimPassword"
+          @input="inputHandler($event, 'confrimPassword')"
+          class="form-input"
+          type="password"
+        >
       </div>
       <div class="form-footer">
         <div @click="create" class="form-footer-wrapper">
@@ -58,17 +93,20 @@ export default {
   data() {
     return {
       user: {
-        firstname: this.state.firstname,
-        lastname: this.state.lastname,
-        login: this.state.login,
-        email: this.state.email,
-        group: this.state.group,
+        firstname: "",
+        lastname: "",
+        login: "",
+        email: "",
+        group: "",
         password: "",
         confrimPassword: ""
       }
     };
   },
   methods: {
+    inputHandler(event, params) {
+      this.user[params] = event.target.value;
+    },
     create() {
       const response = userService.createUser(this.user);
       response
