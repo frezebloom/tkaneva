dcd63b096115084fc30e2d463689f458115e23c7
Mikhail Yarema <juxx@bk.ru>
2019-03-14 21:52:14 +0300

    метод update user "не тестированый"

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -4,6 +4,18 @@ const db = require("../../models/index");
 const User = db.user;
 
 module.exports = {
+  salt: bcrypt.genSaltSync(10),
+
+  get(req, res) {
+    User.findAll()
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(error => {
+        res.status(404).send("Invalid request");
+      });
+  },
+
   create(req, res) {
     const {
       firstname,
@@ -14,8 +26,7 @@ module.exports = {
       password
     } = req.body.user;
 
-    const salt = bcrypt.genSaltSync(10);
-    const passwordToSave = bcrypt.hashSync(password, salt);
+    const passwordToSave = bcrypt.hashSync(password, this.salt);
 
     User.create({
       firstname: firstname,
@@ -33,13 +44,33 @@ module.exports = {
       });
   },
 
-  get(req, res) {
-    User.findAll()
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(error => {
-        res.status(404).send("Invalid request");
-      });
+  update(req, res) {
+    const {
+      id,
+      firstname,
+      lastname,
+      login,
+      email,
+      group,
+      password
+    } = req.body.user;
+
+    const passwordToSave = bcrypt.hashSync(password, this.salt);
+
+    User.update(
+      {
+        firstname,
+        lastname,
+        login,
+        email,
+        group,
+        password: passwordToSave
+      },
+      {
+        where: { id }
+      }
+    ).then(() => {
+      res.status(200).send("updated successfully a customer with id = " + id);
+    });
   }
 };
