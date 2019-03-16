c5f2ed793bf03c476a38492426bb1470e1b37c3e
Mikhail Yarema <juxx@bk.ru>
2019-03-16 12:05:17 +0300

    прокинул в роуты метод updateUser

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 const userController = require("../controllers/api/userController.js");
 
+router.get("/users/get", (req, res) => {
+  userController.get(req, res);
+});
+
 router.post("/user/create", (req, res) => {
   userController.create(req, res);
 });
 
-router.get("/users/get", (req, res) => {
-  userController.get(req, res);
+router.put("/user/update", (req, res) => {
+  userController.update(req, res);
 });
+
 module.exports = router;
