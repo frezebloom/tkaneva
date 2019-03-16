83e13967d0c9d3b3b9167fe7b38a4e0f3142bff5
Mikhail Yarema <juxx@bk.ru>
2019-03-16 20:32:00 +0300

    save

--- dev/manager/src/services/userService.js
+++ dev/manager/src/services/userService.js
@@ -1,12 +1,12 @@
 import api from "@/services/api";
 
 export default {
+  getUsers() {
+    return api().get("/api/users/get");
+  },
   createUser(user) {
     return api().post("/api/user/create", {
       user
     });
-  },
-  getUsers() {
-    return api().get("/api/users/get");
   }
 };
