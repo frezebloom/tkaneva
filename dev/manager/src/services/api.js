b69787056539cf1258e4550c3bde3fa1d695d29e
Mikhail <mikiar@kronshtadt.ru>
2019-07-08 17:02:12 +0300

    save

--- dev/manager/src/services/api.js
+++ dev/manager/src/services/api.js
@@ -1,32 +1,8 @@
 import axios from "axios";
-import store from "../store";
-
-function checkTokenLifeTime(lifeTime) {
-  const currentTime = new Date().getTime()
-  return lifeTime < currentTime ? true : false
-}
 
 export default () => {
-  const { user_id, accessToken, refreshToken, accessTokenLife, refreshTokenLife } = store.getters;
-  let headers;
-  if (checkTokenLifeTime(accessTokenLife)) {
-    if (checkTokenLifeTime(refreshTokenLife)) {
-      console.log('logout');
-    } else {
-      console.log('refresh token')
-    }
-  } else {
-    headers = {
-      user_id,
-      accessToken,
-      refreshToken,
-      accessTokenLife,
-      refreshTokenLife
-    }
-  }
   return axios.create({
     baseURL: "http://localhost:3000",
-    headers: headers
   });
   
   
