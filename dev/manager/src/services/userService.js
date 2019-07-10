7d47d01ec00b84eac02ee94f0ebca513d672bf64
Mikhail <mikiar@kronshtadt.ru>
2019-07-10 16:07:28 +0300

    save

--- dev/manager/src/services/userService.js
+++ dev/manager/src/services/userService.js
@@ -8,16 +8,19 @@ export default {
     return api().get("/api/user/get");
   },
   createUser(user) {
+    token.checkToken();
     return api().post("/api/user/create", {
       user
     });
   },
   updateUser(user) {
+    token.checkToken();
     return api().put("/api/user/update", {
       user
     });
   },
   deleteUser(user) {
+    token.checkToken();
     return api().post("/api/user/delete", {
       user
     });
