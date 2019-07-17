1fdceb5a686cad711546329a56edf849d36c2d86
Mikhail <mikiar@kronshtadt.ru>
2019-07-17 12:41:37 +0300

    проверка токена на время жизни

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -7,6 +7,8 @@ module.exports = {
   return Token.findOne({ where: { user_id: user_id } })
     .then(token => {
       if (token.access_token !== accesstoken) return false;
+      if (token.access_token_life < Date.now()) return false;
+      console.log(user_id, token.access_token, accesstoken)
       return true;
     })
     .catch(() => {
