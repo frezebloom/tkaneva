ecbe86182c396363e7c84c6e8c2151cce51d1a36
Mikhail <mikiar@kronshtadt.ru>
2019-08-20 15:36:09 +0300

    обновление токена для userService

--- dev/manager/src/services/userService.js
+++ dev/manager/src/services/userService.js
@@ -1,26 +1,26 @@
 import api from "@/services/api";
 import token from "@/utils/token";
-// import store from "@/store";
 
 export default {
    async getUsers() {
     const result = await token.checkToken()
     return api(result.id || result.userId, result.accessToken).get("/api/user/get")
   },
-  createUser(user) {
-    return api().post("/api/user/create", {
+  async createUser(user) {
+    const result = await token.checkToken()
+    return api(result.id || result.userId, result.accessToken).post("/api/user/create", {
       user
     });
   },
-  updateUser(user) {
-    token.checkToken();
-    return api().put("/api/user/update", {
+  async updateUser(user) {
+    const result = await token.checkToken()
+    return api(result.id || result.userId, result.accessToken).put("/api/user/update", {
       user
     });
   },
-  deleteUser(user) {
-    token.checkToken();
-    return api().post("/api/user/delete", {
+  async deleteUser(user) {
+    const result = await token.checkToken()
+    return api(result.id || result.userId, result.accessToken).post("/api/user/delete", {
       user
     });
   }
