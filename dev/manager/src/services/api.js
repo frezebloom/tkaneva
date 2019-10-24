8837909d3bce44f51f5a4b4cb5fea7bca1bdd14b
frezebloom <juxx@bk.ru>
2019-10-24 19:38:07 +0000

    save

--- dev/manager/src/services/api.js
+++ dev/manager/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default (id, accessToken) => {
   return axios.create({
-    baseURL: 'http://192.168.0.104:3000',
+    baseURL: 'http://192.168.0.101:3000',
     headers: {
       id,
       accessToken
--- dev/manager/src/views/superuser/CreateSuperUser.vue
+++ dev/manager/src/views/superuser/CreateSuperUser.vue
@@ -16,7 +16,7 @@ export default {
   data() {
     return {
       userGroup: {
-        name: "admin",
+        name: "Пусто",
         status: "Вкл"
       },
       user: {
