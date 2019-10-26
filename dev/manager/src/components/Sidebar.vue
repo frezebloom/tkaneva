4f4f32dc67a4969b6997f0a2caba24bff1079368
frezebloom <juxx@bk.ru>
2019-10-26 21:27:40 +0000

    save

--- dev/manager/src/components/Sidebar.vue
+++ dev/manager/src/components/Sidebar.vue
@@ -21,10 +21,20 @@
       <li class="sidebar-menu-category" @click="open('user')">Учетные записи</li>
       <ul v-bind:class="{ active: isActive.user }" class="sidebar-menu-subcategory">
         <li>
-          <router-link to="/users">Пользователи</router-link>
+          <router-link to="/users">
+            <div class="sidebar-menu-wrapper">
+              <img alt="users" src="../assets/icons/user.svg">
+              <div class="sidebar-menu-title">Пользователи</div>
+            </div>
+          </router-link>
         </li>
         <li>
-          <router-link to="/user-groups">Группы</router-link>
+          <router-link to="/user-groups">
+            <div class="sidebar-menu-wrapper">
+              <img alt="users" src="../assets/icons/user.svg">
+              <div class="sidebar-menu-title">Группы</div>
+            </div>
+          </router-link>
         </li>
       </ul>
     </ul>
@@ -55,53 +65,6 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "../styles/variables.scss";
-.sidebar {
-  display: flex;
-  background-color: $colorBlack;
-  padding-bottom: 10px;
-  width: 230px;
-  height: 100vh;
-  user-select: none;
-}
-.sidebar-menu-header {
-  margin-top: 20px;
-  color: #4b646f;
-  font-size: 14px;
-  background: #1a2226;
-  width: 230px;
-  padding: 8px 8px 8px 15px;
-}
-.sidebar-menu-category {
-  color: #b8c7ce;
-  padding: 12px 55px 12px 15px;
-  display: block;
-  cursor: pointer;
-}
-.sidebar-menu-category:hover {
-  background-color: #1e282c;
-}
-.sidebar-menu-subcategory {
-  display: block;
-  overflow: hidden;
-  background-color: #2c3b41;
-  max-height: 0px;
-
-  transition: max-height 1s;
-}
-.sidebar-menu-subcategory a {
-  padding: 5px 5px 5px 15px;
-  display: block;
-  font-size: 14px;
-  color: #b8c7ce;
-}
-.sidebar-menu-subcategory a:hover {
-  color: $colorWhite;
-}
-.sidebar-menu-subcategory a:before {
-  content: "⚬ ";
-}
-.active {
-  max-height: 100%;
-}
+@import "../styles/sidebar.scss";
 </style>
 
