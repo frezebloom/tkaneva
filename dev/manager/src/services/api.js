1181f88caa4bfaf4050d6e8c10094dde6d41452c
Yarema Mikhail <mi@194-58-98-72.cloudvps.regruhosting.ru>
2019-11-03 14:50:38 +0300

    save

--- dev/manager/src/services/api.js
+++ dev/manager/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default (id, accessToken) => {
   return axios.create({
-    baseURL: 'http://192.168.0.102:3000',
+    baseURL: 'http://194.58.98.72:3000',
     headers: {
       id,
       accessToken
