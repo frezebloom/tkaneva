a724178800d52cc7498e51a54c5499eadf02c03d
frezebloom <juxx@bk.ru>
2019-11-07 17:19:43 +0000

    save

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -1,16 +1,16 @@
-const db = require("../models/index");
+const db = require('../models/index');
 const Token = db.token;
 
 module.exports = {
   checkToken(id, accesstoken) {
-  return Token.findOne({ where: { user_id: id } })
-    .then(token => {
-      if (token.access_token !== accesstoken) return false;
-      if (token.access_token_life < Date.now()) return false;
-      return true;
-    })
-    .catch(() => {
-      return false;
-    })
+    return Token.findOne({ where: { user_id: id } })
+      .then(token => {
+        if (token.access_token !== accesstoken) return false;
+        if (token.access_token_life < Date.now()) return false;
+        return true;
+      })
+      .catch(() => {
+        return false;
+      });
   }
-}
+};
