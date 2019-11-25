7e39773dcf7128e14bee2127679fbde39dd78883
frezebloom <juxx@bk.ru>
2019-11-25 18:27:24 +0000

    save

---
+++ dev/manager/src/views/products/NewProduct.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="new-product">
+    <ProductForm title="Создать продукт" :state="user" />
+  </div>
+</template>
+<script>
+import ProductForm from "@/components/ProductForm";
+import services from "@/services/services";
+
+export default {
+  name: "NewProduct",
+  components: {
+    ProductForm
+  },
+  data() {
+    return {
+      product: {
+        article: "",
+        name: "",
+        category: "",
+        properties: {
+          structure: "",
+          brand: "",
+          color: "",
+          width: "",
+          length: ""
+        },
+        price: "",
+        discount: "",
+        description: "",
+        status: ""
+      }
+    };
+  }
+};
+</script>
