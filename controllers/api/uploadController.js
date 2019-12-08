6b6ffb365c5b3e37388a0b9d99f74d9016ccaf62
frezebloom <juxx@bk.ru>
2019-12-08 12:33:56 +0000

    upload image server

--- controllers/api/uploadController.js
+++ controllers/api/uploadController.js
@@ -1,5 +1,18 @@
+const path = require("path");
+const Resize = require("../../utils/resize");
+
 module.exports = {
   render: function(req, res) {
     res.render("uploadTest", { title: "Hey", message: "Hello there!" });
+  },
+
+  upload: async function(req, res) {
+    const imagePath = path.join(__dirname, "/../../public/images/products");
+    const fileUpload = new Resize(imagePath);
+    if (!req.file) {
+      res.status(401).json({ error: "Please provide an image" });
+    }
+    const filename = await fileUpload.save(req.file.buffer);
+    return res.status(200).json({ name: filename });
   }
 };
--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const uploadMiddleware = require("../../utils/middleware/uploadMiddleware");
 const uploadController = require("../../controllers/api/uploadController");
 
 router.get("/upload/image", (req, res) => {
   uploadController.render(req, res);
+});
+
+router.post("/upload/image", uploadMiddleware.single("image"), (req, res) => {
+  uploadController.upload(req, res);
 });
 
 module.exports = router;
