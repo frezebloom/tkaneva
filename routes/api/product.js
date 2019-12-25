58da02e928782a12738b29e87e361bf1d57ebdb7
frezebloom <juxx@bk.ru>
2019-12-25 19:37:36 +0000

    router product

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const superUserController = require("../../controllers/api/superUserController");
+const productController = require("../../controllers/api/productController");
+const tokenMiddleware = require("../../middlewares/token");
 
-router.post("/superuser/user/create", (req, res) => {
-  superUserController.createSuperUser(req, res);
+router.get("/product/get", (req, res) => {
+  productController.get(req, res);
 });
 
-router.post("/superuser/user-group/create", (req, res) => {
-  superUserController.createSuperUserGroup(req, res);
-});
+router.post(
+  "/product/create",
+  (req, res, next) => tokenMiddleware.checkToken(req, res, next),
+  (req, res) => productController.create(req, res)
+);
+
+router.put(
+  "/product/update",
+  (req, res, next) => tokenMiddleware.checkToken(req, res, next),
+  (req, res) => productController.update(req, res)
+);
+
+router.post(
+  "/product/delete",
+  (req, res, next) => tokenMiddleware.checkToken(req, res, next),
+  (req, res) => productController.delete(req, res)
+);
 
 module.exports = router;
