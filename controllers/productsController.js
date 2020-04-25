a5a06229533263ad7ff5dd0656d5bea6f236c641
frezebloom <juxx@bk.ru>
2020-04-25 18:29:58 +0000

    save

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,33 @@
+const db = require("../models/index");
+const Product = db.product;
+const Category = db.category;
+const Brand = db.brand;
+const Color = db.color;
+
 module.exports = {
-  render: function(req, res) {
-    res.render("index", { title: "Hey", message: "Hello there!" });
-  }
+  getProducts: function () {
+    return Product.findAll({
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Brand,
+        },
+        {
+          model: Color,
+        },
+      ],
+    });
+  },
+  getCategories: function () {
+    return Category.findAll();
+  },
+  render: function (req, res) {
+    Promise.all([this.getCategories()]).then((values) => {
+      const categories = values[0].map((category) => category.dataValues);
+      console.log(categories);
+      res.render("index", { categories });
+    });
+  },
 };
