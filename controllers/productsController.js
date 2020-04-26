08478c59054318ddf33a03fc23f997f127644e62
frezebloom <juxx@bk.ru>
2020-04-26 13:07:57 +0000

    save

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,12 @@
+const path = require("path");
 const db = require("../models/index");
 const Product = db.product;
 const Category = db.category;
 const Brand = db.brand;
 const Color = db.color;
+const Upload = db.upload;
+
+const publicPath = path.join(__dirname, "/../../public/images/products");
 
 module.exports = {
   getProducts: function () {
@@ -20,14 +24,47 @@ module.exports = {
       ],
     });
   },
+
+  getFilesPath: function (directory, uploadId) {
+    return Upload.findAll({
+      where: {
+        upload_id: uploadId,
+      },
+    })
+      .then((uploads) => {
+        Promise.resolve(uploads.map(({ name }) => `${directory}/${name}`));
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(404).send("Invalid request" + error);
+      });
+  },
+
   getCategories: function () {
     return Category.findAll();
   },
+
   render: function (req, res) {
-    Promise.all([this.getCategories()]).then((values) => {
-      const categories = values[0].map((category) => category.dataValues);
-      console.log(categories);
-      res.render("index", { categories });
+    const tasks = [this.getProducts, this.getFilesPath];
+
+    const a = tasks.reduce((acc, fn) => acc.then(fn), Promise.resolve());
+
+    a.then((result) => {
+      console.log(result);
     });
+
+    //   Promise.all([this.getProducts(), this.getCategories()]).then((values) => {
+    //     const products = values[0].map((product) => {
+    //       const { article, uploads_id } = product.dataValues;
+    //       const directory = `${publicPath}/${article}`;
+
+    //       this.getFilesPath(directory, JSON.parse(uploads_id)).then((path) => {
+    //         return product.dataValues;
+    //       });
+    //     });
+    //     const categories = values[1].map((category) => category.dataValues);
+    //     res.render("index", { products, categories });
+    //   });
+    // },
   },
 };
